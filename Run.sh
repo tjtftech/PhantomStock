9e046fca54df80508d15490cf04cbce9cc9c069c
tjtftech <postmaster@tjtftech.com>
2018-06-27 14:55:03 +0800

    add

--- Run.sh
+++ Run.sh
@@ -1,1 +1,1 @@
-cd /Users/tfkj/work/ipa && git commit -a -m "add" && git push origin master && echo "更新到github成功"
+cd /Users/liujie/work/ipa/ipas && git commit -a -m "add" && git push origin master && echo "更新到github成功"
